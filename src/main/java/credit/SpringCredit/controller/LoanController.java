4ddc938f2dd434c1864ae79c0a90e639239d108d
annov4 <137298843+annov4@users.noreply.github.com>
2024-05-06 02:04:56 +0300

    pu-pu-pu

--- src/main/java/credit/SpringCredit/controller/LoanController.java
+++ src/main/java/credit/SpringCredit/controller/LoanController.java
@@ -1,10 +1,12 @@
 package credit.SpringCredit.controller;
 
 
+import credit.SpringCredit.model.User;
 import credit.SpringCredit.service.CreditCalculator;
 import credit.SpringCredit.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -25,9 +27,17 @@ public class LoanController {
 
 
     @GetMapping
-    public String approveLoan(@RequestParam(value = "userId") Long userId) {
-//реализация
-        return "credit";
-    }
+    public String approveLoan(@RequestParam(value = "userId") Long userId, Model model) {
+        User userWithIncome = userService.getUserWithIncome(userId);
+        double approvedAmount = creditCalculator.calculateApprovedAmount(userWithIncome);
 
+        model.addAttribute("user", userWithIncome);
+        model.addAttribute("approvedAmount", approvedAmount);
+
+        if (approvedAmount > 0) {
+            return "loanApproved";
+        } else {
+            return "loanRejected";
+        }
+    }
 }
