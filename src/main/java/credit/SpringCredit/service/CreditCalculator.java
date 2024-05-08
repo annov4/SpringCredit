65ac1af08118e00577076409047e5da3e9318547
annov4 <137298843+annov4@users.noreply.github.com>
2024-05-09 01:25:35 +0300

    create LoanProperties.java

--- src/main/java/credit/SpringCredit/service/CreditCalculator.java
+++ src/main/java/credit/SpringCredit/service/CreditCalculator.java
@@ -1,31 +1,30 @@
 package credit.SpringCredit.service;
 
+import credit.SpringCredit.config.LoanProperties;
 import credit.SpringCredit.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CreditCalculator {
-
-    @Value("${loan.minimalIncome}")
-    private int minimalIncome;
-
-    @Value("${loan.minCarValue}")
-    private int minCarValue;
+    private final LoanProperties loanProperties;
+    private final IncomeService incomeService;
 
     @Autowired
-    private IncomeService incomeService;
+    public CreditCalculator(LoanProperties loanProperties, IncomeService incomeService) {
+        this.loanProperties = loanProperties;
+        this.incomeService = incomeService;
+    }
 
     public double calculateApprovedAmount(User user) {
         double income = incomeService.getIncomeForUser(user.getId()).getIncome();
         double approvedAmount = 0;
 
-        if (user.getCar() != null && user.getCar().getPrice() > minCarValue) {
-            approvedAmount = user.getCar().getPrice() * 0.3;
+        if (user.getCar() != null && user.getCar().getPrice() > loanProperties.getMinCarValue()) {
+            approvedAmount = user.getCar().getPrice() * loanProperties.getPercentOfCarValue();
         }
-        if (income > minimalIncome) {
-            approvedAmount = Math.max(approvedAmount, income * 12 * 0.5);
+        if (income > loanProperties.getMinimalIncome()) {
+            approvedAmount = Math.max(approvedAmount, income * 12 * loanProperties.getPercentOfIncome());
         }
 
         return approvedAmount;
