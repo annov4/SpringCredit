aaf7e8d2cddd112dde5baf00598a491328188cfd
annov4 <137298843+annov4@users.noreply.github.com>
2024-05-16 13:32:35 +0300

    add lombok

--- src/main/java/credit/SpringCredit/service/CreditCalculator.java
+++ src/main/java/credit/SpringCredit/service/CreditCalculator.java
@@ -3,19 +3,16 @@ package credit.SpringCredit.service;
 import com.example.spring_boot_starter.client.IncomeClient;
 import credit.SpringCredit.config.LoanProperties;
 import credit.SpringCredit.model.User;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class CreditCalculator {
 
     private final LoanProperties loanProperties;
     private final IncomeClient incomeClient;
 
-
-    public CreditCalculator(LoanProperties loanProperties, IncomeClient incomeClient) {
-        this.loanProperties = loanProperties;
-        this.incomeClient = incomeClient;
-    }
 
     public double calculateApprovedAmount(User user) {
         double income = incomeClient.getIncomeForUser(user.getId()).getIncome();
